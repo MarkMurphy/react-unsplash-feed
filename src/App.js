92b2c86013f49f1019d46c36f07da638e2bb6caf
Mark Murphy <mmurphy@kinduct.com>
2018-09-13 17:31:15 -0300

    Add image feed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AppHeader from "./AppHeader";
+import AppIntro from "./AppIntro";
+import ImageList from "./ImageList";
+
+const accessKey =
+  "7e8948b261ea2a28cdafe10db9c7b3d07b8a1ff1feaf33b909618a6eceefa919";
 
 class App extends Component {
+  state = {
+    images: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch(
+      "https://api.unsplash.com/photos?page=1&per_page=9",
+      {
+        headers: {
+          Authorization: `Client-ID ${accessKey}`
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    this.setState({ images: data });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+        <AppHeader image={logo} />
+        <AppIntro>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </AppIntro>
+        <ImageList images={this.state.images} />
       </div>
     );
   }
