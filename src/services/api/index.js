e389cdfebaafa4bba41808ce6ea079a2149c5ad2
Mark Murphy <mmurphy@kinduct.com>
2018-10-18 12:28:47 -0300

    Update api client

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,14 +1,14 @@
 const ACCESS_KEY =
-  "7e8948b261ea2a28cdafe10db9c7b3d07b8a1ff1feaf33b909618a6eceefa919";
+  '7e8948b261ea2a28cdafe10db9c7b3d07b8a1ff1feaf33b909618a6eceefa919';
 
-const BASE_URL = "https://api.unsplash.com/";
+const BASE_URL = 'https://api.unsplash.com/';
 
 const querystring = {
   stringify(params) {
     return Object.keys(params)
-      .map(k => encodeURIComponent(k) + "=" + encodeURIComponent(params[k]))
-      .join("&");
-  }
+      .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+      .join('&');
+  },
 };
 
 export const request = async (url, params, options = {}) => {
@@ -18,7 +18,7 @@ export const request = async (url, params, options = {}) => {
 
   if (params) {
     if (!options.method || /GET/i.test(options.method)) {
-      url += "?" + querystring.stringify(params);
+      url += '?' + querystring.stringify(params);
     } else {
       options.body = params;
     }
@@ -26,7 +26,7 @@ export const request = async (url, params, options = {}) => {
 
   options.headers = {
     Authorization: `Client-ID ${ACCESS_KEY}`,
-    ...options.headers
+    ...options.headers,
   };
 
   const response = await fetch(url, options);
@@ -35,11 +35,11 @@ export const request = async (url, params, options = {}) => {
 };
 
 export const get = async (url, params, options) => {
-  return request(url, params, { ...options, method: "GET" });
+  return request(url, params, { ...options, method: 'GET' });
 };
 
 export const post = async (url, params, options) => {
-  return request(url, params, { ...options, method: "POST" });
+  return request(url, params, { ...options, method: 'POST' });
 };
 
 export default { get, post, request };
